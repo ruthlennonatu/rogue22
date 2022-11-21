4d8b174df993fc686162eed3115c579d13e8e568
Thomas Bridgeman <115080492+thomasbridgemanatu@users.noreply.github.com>
2022-11-21 19:33:23 +0000

    Constructor and getter methods for Workshop class

--- Code/src/main/java/atu/Workshop.java
+++ Code/src/main/java/atu/Workshop.java
@@ -1,5 +1,48 @@
 package main.java.atu;
 
+/**
+ * Workshop class for comic booking system
+ * @author RogueATU
+ */
 public class Workshop {
+    /**
+     * The name of the user they will access the system with
+     */
+    private String name;
+    /**
+     * The number of possible attendees
+     */
+    private int attendees;
 
+
+    /**
+     * Constructor for User
+     *
+     * @param name      the name of the workshop
+     * @param attendees the amount of possible places
+     */
+    public Workshop(String name, int attendees) {
+        //setName(name);
+        //setAttendees(attendees);
+
+    }
+
+    /**
+     * Getter for Name
+     *
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Getter for Attendees
+     *
+     * @return Attendees
+     */
+    public int getAttendees() {
+        return attendees;
+    }
 }
+
