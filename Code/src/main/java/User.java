cbed7af80d356188d1669c6540a36a64c75a1f25
Thomas Bridgeman <115080492+thomasbridgemanatu@users.noreply.github.com>
2022-11-30 22:41:49 +0000

    File Structure For All Code Going Forward

---
+++ Code/src/main/java/User.java
@@ -0,0 +1,43 @@
+/**
+ * User class for comic booking system
+ * @author RogueATU
+ */
+public class User {
+    /**
+     *The name of the user they will access the system with
+     */
+    private String username;
+
+    /**
+     * Constructor for User
+     * @param username the username of the User
+     */
+    public User(String username){
+        setUsername(username);
+    }
+
+    /**
+     * Getter for username
+     * @return username
+     */
+    public String getUsername(){
+        return username;
+    }
+
+    /**
+     * Setter for username
+     * @param username the username of the User
+     */
+    public void setUsername(String username){
+        this.username=username;
+    }
+
+    /**
+     * To string method for User
+     * @return username
+     */
+    @Override
+    public String toString(){
+        return username;
+    }
+}
